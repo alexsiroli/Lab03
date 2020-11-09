1282686eb92c12a580591ccbf2468689f3600c20
Alex Siroli <alex.siroli@studio.unibo.it>
2020-11-09 22:06:58 +0100

    Costruzione di classi inclapsulate - fatto

--- src/oop/lab03/bank/SimpleBankAccount.java
+++ src/oop/lab03/bank/SimpleBankAccount.java
@@ -2,7 +2,6 @@ package oop.lab03.bank;
 
 public class SimpleBankAccount {
 
-	private int userID;
     /*
      * Aggiungere i seguenti campi:
      * - double balance: ammontare del conto
@@ -11,11 +10,22 @@ public class SimpleBankAccount {
      * - static double ATM_TRANSACTION_FEE = 1: costo delle operazioni via ATM
      */
 
+	private static double ATM_TRANSACTION_FEE = 1;
+	private double balance;
+	private int userID;
+	private int nTransactions;
+
     /*
      * Creare un costruttore pubblico che prenda in ingresso un intero (ossia l'id
      * dell'utente) ed un double (ossia, l'ammontare iniziale del conto corrente).
      */
 
+	public SimpleBankAccount(int userID, double balance) {
+		this.balance = balance;
+		this.userID = userID;
+		this.nTransactions = 0;
+	}
+	
     /*
      * Si aggiungano selettori per: 
      * - ottenere l'id utente del possessore del conto
@@ -23,20 +33,42 @@ public class SimpleBankAccount {
      * - ottenere l'ammontare corrente del conto.
      */
 
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public int getnTransactions() {
+		return nTransactions;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	private void transaction(int usrID, double amount) {
+		if (this.checkUser(usrID) ) {
+			this.nTransactions ++;
+			this.balance = this.balance + amount;
+		}
+	}
+		
     public void deposit(final int usrID, final double amount) {
         /*
          * Incrementa il numero di transazioni e aggiunge amount al totale del
          * conto Nota: il deposito va a buon fine solo se l'id utente
          * corrisponde
          */
+    	this.transaction(usrID, amount); 	
     }
 
-    public void withdraw(final int usrID, final double amount) {
+	public void withdraw(final int usrID, final double amount) {
         /*
          * Incrementa il numero di transazioni e rimuove amount al totale del
          * conto. Note: - Il conto puo' andare in rosso (ammontare negativo) -
          * Il prelievo va a buon fine solo se l'id utente corrisponde
          */
+		this.transaction(usrID, -amount);
     }
 
     public void depositFromATM(final int usrID, final double amount) {
@@ -46,6 +78,7 @@ public class SimpleBankAccount {
          * all'uso dell'ATM (bancomat) Nota: il deposito va a buon fine solo se
          * l'id utente corrisponde
          */
+    	this.transaction(usrID, amount-ATM_TRANSACTION_FEE);
     }
 
     public void withdrawFromATM(final int usrID, final double amount) {
@@ -56,10 +89,16 @@ public class SimpleBankAccount {
          * negativo) - Il prelievo va a buon fine solo se l'id utente
          * corrisponde
          */
+    	this.transaction(usrID, -amount-ATM_TRANSACTION_FEE);
     }
 
     /* Utility method per controllare lo user */
     private boolean checkUser(final int id) {
         return this.userID == id;
     }
+    
+    public String toString() {
+		return "SimpleBankAccount [balance=" + balance + ", userID=" + userID + ", nTransactions=" + nTransactions
+				+ "]";
+	}
 }
--- src/oop/lab03/bank/TestSimpleBankAccount.java
+++ src/oop/lab03/bank/TestSimpleBankAccount.java
@@ -5,13 +5,44 @@ public final class TestSimpleBankAccount {
     private TestSimpleBankAccount() { }
 
     public static void main(final String[] args) {
-        /*
-         * 1) Creare l' AccountHolder relativo a Mario Rossi con id 1 2) Creare
-         * l' AccountHolder relativo a Luigi Bianchi con id 2 3) Creare i due
-         * SimpleBankAccount corrispondenti 4) Effettuare una serie di depositi e
-         * prelievi 5) Stampare a video l'ammontare dei due conti e verificare
-         * la correttezza del risultato 6) Provare a prelevare fornendo un idUsr
-         * sbagliato 7) Controllare nuovamente l'ammontare
-         */
+
+        // 1) Creare l' AccountHolder relativo a Mario Rossi con id 1
+    	
+    	AccountHolder ah1 = new AccountHolder("Mario", "Rossi", 1);
+    	
+    	// 2) Creare l' AccountHolder relativo a Luigi Bianchi con id 2
+    	
+    	AccountHolder ah2 = new AccountHolder("Luigi", "Bianchi", 2);
+    	
+    	// 3) Creare i due SimpleBankAccount corrispondenti
+    	
+    	SimpleBankAccount sb1 = new SimpleBankAccount(ah1.getUserID(), 0);
+    	SimpleBankAccount sb2 = new SimpleBankAccount(ah2.getUserID(), 0);
+    	
+    	// 4) Effettuare una serie di depositi e prelievi
+    	
+    	sb1.deposit(1, 1_000);
+    	sb1.withdraw(1, 500);
+    	sb1.depositFromATM(1, 100);
+    	sb1.withdrawFromATM(1, 200);
+    	
+    	sb2.deposit(2, 10_000);
+    	sb2.withdraw(2, 9_000);
+    	sb2.depositFromATM(2, 50_000);
+    	sb2.withdrawFromATM(2, 1_000);
+    	
+        // 5) Stampare a video l'ammontare dei due conti e verificare la correttezza del risultato
+    	
+    	System.out.println(sb1.toString());
+    	System.out.println(sb2.toString());
+    	
+        // 6) Provare a prelevare fornendo un idUsr sbagliato
+    	
+    	sb1.withdraw(3, 500);
+    	
+        // 7) Controllare nuovamente l'ammontare
+		
+    	System.out.println(sb1.toString());
+    	
     }
 }
